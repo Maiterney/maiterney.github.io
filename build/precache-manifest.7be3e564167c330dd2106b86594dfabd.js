849ac59879590d307f718007f2858595c19ea13c
Maiterney <43651018+Maiterney@users.noreply.github.com>
2019-11-19 21:21:30 -0300

    sending Build
    sending Build

---
+++ build/precache-manifest.7be3e564167c330dd2106b86594dfabd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2ef95432ad3e9a49086baf6386a7d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71596e11bf2e6de776a2",
+    "url": "/static/css/main.042cab39.chunk.css"
+  },
+  {
+    "revision": "3b0581063136b4a27651",
+    "url": "/static/js/2.6eec4c8f.chunk.js"
+  },
+  {
+    "revision": "71596e11bf2e6de776a2",
+    "url": "/static/js/main.0639a827.chunk.js"
+  },
+  {
+    "revision": "df67b54b1e6c71c6ffdf",
+    "url": "/static/js/runtime~main.5a671027.js"
+  },
+  {
+    "revision": "5083e0a2a7dcaae07c142e8b87036a27",
+    "url": "/static/media/about_img.5083e0a2.gif"
+  },
+  {
+    "revision": "a384abd44ba16116b3146ecbf0ff0371",
+    "url": "/static/media/coding.a384abd4.svg"
+  },
+  {
+    "revision": "06f4431667beec93a0d63689b5f402e6",
+    "url": "/static/media/gamepad.06f44316.svg"
+  },
+  {
+    "revision": "d11f1926dd50104a659e0acc40c6d536",
+    "url": "/static/media/mouse.d11f1926.svg"
+  },
+  {
+    "revision": "e8e2a09f6658d5e87b8863a03c545956",
+    "url": "/static/media/pen.e8e2a09f.svg"
+  },
+  {
+    "revision": "532cc05ca24903c6b3f88af1580dc593",
+    "url": "/static/media/smartphone.532cc05c.svg"
+  },
+  {
+    "revision": "a1ffa5d953ee3f4ce61c492f5bbf7fd2",
+    "url": "/static/media/tecnologies.a1ffa5d9.jpg"
+  }
+]);
